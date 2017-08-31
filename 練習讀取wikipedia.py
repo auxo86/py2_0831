70ecf568b8688b98e220a0d4411476f4a5b699ec
auxo <auxo_86@hotmail.com>
2017-08-31 21:10:04 +0800

    練習透過網路抓取wikipedia的資料-2

--- 練習讀取wikipedia.py
+++ 練習讀取wikipedia.py
@@ -4,3 +4,11 @@ import wikipedia
 
 print wikipedia.summary('jetbrain')
 print wikipedia.search('C++')
+taipei = wikipedia.page("Taipei")
+print taipei.title, taipei.url
+print taipei.content
+print taipei.links[0]
+
+
+wikipedia.set_lang('zh-tw')
+print wikipedia.summary('Taipei', sentences=4)
